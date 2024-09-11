381e701fac41b472b22653992484c278a5225385
kamil-homer <kamilhomer@gmail.com>
2024-09-11 14:40:40 +0200

    setup

---
+++ layouts/DashboardWrapper/DashboardWrapper.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from "react";
+
+const DashboardWrapper = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className={`flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+      Sidebar d
+      <main
+        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}
+      >
+        {children}
+        Main
+      </main>
+    </div>
+  );
+};
+
+export default DashboardWrapper;
