d9b894724fe8ed4f078b8a2f9215dab72a57775d
kamil-homer <kamilhomer@gmail.com>
2024-09-12 16:16:12 +0200

    navbar

--- layouts/DashboardWrapper/DashboardWrapper.tsx
+++ layouts/DashboardWrapper/DashboardWrapper.tsx
@@ -1,12 +1,14 @@
+import Navbar from "@/components/Navbar/Navbar";
 import React, { ReactNode } from "react";
 
 const DashboardWrapper = ({ children }: { children: ReactNode }) => {
   return (
-    <div className={`flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
-      Sidebar d
+    <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+      Sidebar
       <main
         className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}
       >
+        <Navbar />
         {children}
         Main
       </main>
